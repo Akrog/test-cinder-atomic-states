e4fb3715023b938d99bcc2753786a8a71858586e
Gorka Eguileor <geguileo@redhat.com>
2015-04-09 16:18:49 +0200

    Check changed data on all servers, not just through HAproxy

--- ha_test.py
+++ ha_test.py
@@ -11,17 +11,29 @@ def my_func():
     
     return {'args': args, 'kwargs': kwargs}
 
-def check_volume(session, vol_id, data):
-    vol = session.query(db.Volume).get(vol_id)
-    for k, v in data.iteritems():
-        d = getattr(vol, k)
-        if d != v:
-            raise Exception('Wrong data in %s, key %s %s != %s' % (vol_id, k, v, d))
+def check_volume(nodes, vol_id, data):
+    for node in nodes:
+        vol = node[1].query(db.Volume).get(vol_id)
+        for k, v in data.iteritems():
+            d = getattr(vol, k)
+            if d != v:
+                raise Exception('Wrong data in server %s in %s, key %s %s != %s' % (node[2], vol_id, k, v, d))
 
 def do_test(worker_id, db_data, changer, session_cfg={}, *args, **kwargs):
+    node_ips = db_data.pop('node_ips', [])
+
+    db_cfg = db_data.copy()
+    nodes = []
+    for node_ip in node_ips:
+        db_cfg['ip'] = node_ip
+        database = db.Db(**db_data)
+        session = database.session
+        nodes.append((database, session, node_ip))
+
     db_data['session_cfg'] = session_cfg
     database = db.Db(**db_data)
     session = database.session
+       
     results = []
     vol_id = database.current_uuids[0]
     #return vol_id
@@ -29,7 +41,7 @@ def do_test(worker_id, db_data, changer, session_cfg={}, *args, **kwargs):
         try:
             marker = '%s_%s' % (worker_id, i) 
             changer(session, vol_id, 'available', 'deleting', marker)
-            check_volume(session, vol_id, {'status': 'deleting', 'attach_status': marker})
+            check_volume(nodes, vol_id, {'status': 'deleting', 'attach_status': marker})
             changer(session, vol_id, 'deleting', 'available', marker)
             results.append('Ok %s' % i)
         except Exception as e:
@@ -42,6 +54,8 @@ if __name__ == '__main__':
     database.create_table()
     database.populate()
     database.close()
+
+    db_data['node_ips'] = ['192.168.1.15', '192.168.1.16', '192.168.1.17']
 
     t = Tester(do_test, None, db_data, deadlock.make_change, deadlock.session_cfg, database.current_uuids[0])
     #t = Tester(do_test, None, database.get_engine(), deadlock.make_change, database.current_uuids[0])
