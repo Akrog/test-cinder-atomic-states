2a67d365bcaa078ab605a43e257e75d391fac6ec
Gorka Eguileor <geguileo@redhat.com>
2015-04-09 17:01:09 +0200

    Deadlock solution with select for update is working

--- solutions/deadlock.py
+++ solutions/deadlock.py
@@ -1,8 +1,29 @@
 import db
+import functools
 import time
+from sqlalchemy.exc import OperationalError
 
 session_cfg = {'autocommit': False, 'expire_on_commit': True}
 
+
+def _retry_on_deadlock(f):
+    """Decorator to retry a DB API call if Deadlock was received."""
+    @functools.wraps(f)
+    def wrapped(session, *args, **kwargs):
+        while True:
+            try:
+                return f(session, *args, **kwargs)
+            except OperationalError as e:
+                if not e.args[0].startswith("(OperationalError) (1213, 'Deadlock found"):
+                    raise
+                session.rollback()
+                # Retry!
+                time.sleep(0.5)
+                continue
+    functools.update_wrapper(wrapped, f)
+    return wrapped
+
+@_retry_on_deadlock
 def make_change(session, vol_id, initial, destination, attach_status):
     i = 0
     while True:
@@ -20,5 +41,3 @@ def make_change(session, vol_id, initial, destination, attach_status):
     vol.status = destination
     vol.attach_status = attach_status
     session.commit()
-    
-    # return "I'm session %s id %s status from %s to %s attach_status %s" % (session, id, initial, destination, attach_status)
