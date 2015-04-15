a1bca0bdb2875ec164c45a799447c3ee093c23a2
Gorka Eguileor <geguileo@redhat.com>
2015-04-15 21:50:58 +0200

    Simplify tooz file test

--- solutions/tooz_file.py
+++ solutions/tooz_file.py
@@ -6,7 +6,7 @@ from tooz import coordination
 import os
 
 
-session_cfg = {'autocommit': False, 'expire_on_commit': True}
+session_cfg = {'autocommit': True, 'expire_on_commit': True}
 
 
 def _retry_on_deadlock(f):
@@ -37,29 +37,16 @@ def make_change(session, vol_id, initial, destination, attach_status):
     global lock
     if coordinator is None:
         coordinator = coordination.get_coordinator('file://' + os.getcwd(), str(session))
+        #coordinator = coordination.get_coordinator('redis://192.168.1.14', str(session))
+        #coordinator = coordination.get_coordinator('ipc://' + os.getcwd(), str(session))
         coordinator.start()
         lock = coordinator.get_lock(vol_id)
-    try:
-        while True:
-            #vol = session.query(db.Volume).with_for_update().get(vol_id)
-            #with coordinator.get_lock(vol_id):
-            if not lock.acquire():
-                continue
-            vol = session.bind.execute("select * from volumes where id='" + vol_id + "'").first()
-            #print 'vol', list(vol.iterkeys()), list(vol.itervalues())
+    while True:
+        with lock, session.begin():
+            vol = session.query(db.Volume).with_for_update().get(vol_id)
             if vol.status == initial:
-                #print "We're in"
-                try:
-                    vol = session.query(db.Volume).get(vol_id)
-                    vol.status = destination
-                    vol.attach_status = attach_status
-                    session.commit()
-                    return
-                except:
-                   session.rollback()
-                   raise
-            lock.release()
-            time.sleep(0.01)
-    finally:
-        lock.release()
-        #coordinator.stop()
+                vol.status = destination
+                vol.attach_status = attach_status
+                return
+        coordinator.heartbeat()
+        time.sleep(0.01)
